a1e69cd21c41bc7ec707235611fc2a11b90d7b9a
vanpipy <vanpipy@gmail.com>
2022-05-21 20:52:40 +0800

    feat: add a mediator pattern description

---
+++ packages/design-pattern/src/Mediator.ts
@@ -0,0 +1,18 @@
+/**
+ * [Mediator Pattern](https://refactoring.guru/design-patterns/mediator)
+ *
+ * There is an important definition of the mediator that:
+ * > Mediator is a behavioral design pattern that lets you reduce chaotic dependencies between
+ * > objects. The pattern restricts direct communications between the objects and forces them to
+ * > collaborate only via a mediator object.
+ *
+ * means a mediator changes the relations between the objects, it is very simple, useful, and
+ * not easy to find an example when coding cause its simple. The appearance is easy to confuse
+ * and blind us, the mediator pattern is a good way to think and review the passed.
+ *
+ * For this, the mediator pattern is not a immutable structure, its structure depends the
+ * environment used.
+ *
+ * So coding a mediator without real stuff like building a house but no one in.
+ */
+
