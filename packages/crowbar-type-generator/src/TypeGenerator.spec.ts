237859ddf6787541f9a86642050d56e624d397fe
vanpipy <vanpipy@gmail.com>
2025-05-21 11:15:34 +0800

    feat(type-generator): add type generator

---
+++ packages/crowbar-type-generator/src/TypeGenerator.spec.ts
@@ -0,0 +1,91 @@
+import { describe, expect, it, jest, beforeEach } from '@jest/globals';
+import { swaggerGenerate } from './helper';
+import TypeGenerator from './TypeGenerator';
+
+jest.mock('fs/promises');
+jest.mock('./helper', () => ({
+  swaggerGenerate: jest.fn(),
+  request: jest.fn().mockImplementation(() => ({
+    json: () => ({ a: 1 }),
+  })),
+}));
+
+const cwd = process.cwd();
+
+describe('TypeGenerator', () => {
+  beforeEach(() => {
+    jest.useFakeTimers().setSystemTime(new Date('1970-01-01'));
+  });
+
+  it('should catch an empty uri exception when the uri is invalid', () => {
+    // eslint-disable-next-line
+    // @ts-ignore
+    const create = () => new TypeGenerator();
+    const createAgain = () => new TypeGenerator({ uri: '' });
+    expect(() => create()).toThrow('The uri is required for TypeGenerator');
+    expect(() => createAgain()).toThrow('The uri is required for TypeGenerator');
+  });
+
+  it('should be invoked the default config', async () => {
+    const typeGenerator = new TypeGenerator({ uri: 'https://domain' });
+    await typeGenerator.generate();
+    expect(swaggerGenerate).toHaveBeenCalledWith({
+      input: `${cwd}/.temp-0.json`,
+      output: `${cwd}/swagger`,
+      moduleNameFirstTag: true,
+      moduleNameIndex: 1,
+      unwrapResponseData: true,
+      extractRequestParams: true,
+      extractRequestBody: true,
+      extractResponseBody: true,
+      extractResponseError: true,
+      extractEnums: true,
+      sortTypes: true,
+      sortRoutes: true,
+      patch: true,
+      httpClientType: 'axios',
+      generateClient: true,
+      generateResponses: true,
+      generateRouteTypes: true,
+      generateUnionEnums: true,
+      // eslint-disable-next-line
+      // @ts-ignore
+      modular: true,
+    });
+  });
+
+  it('should set an overwrited output', async () => {
+    const typeGenerator = new TypeGenerator({ uri: 'https://domain', output: './swagger' });
+    await typeGenerator.generate();
+    expect(swaggerGenerate).toHaveBeenCalledWith({
+      input: `${cwd}/.temp-0.json`,
+      output: './swagger',
+      moduleNameFirstTag: true,
+      moduleNameIndex: 1,
+      unwrapResponseData: true,
+      extractRequestParams: true,
+      extractRequestBody: true,
+      extractResponseBody: true,
+      extractResponseError: true,
+      extractEnums: true,
+      sortTypes: true,
+      sortRoutes: true,
+      patch: true,
+      httpClientType: 'axios',
+      generateClient: true,
+      generateResponses: true,
+      generateRouteTypes: true,
+      generateUnionEnums: true,
+      // eslint-disable-next-line
+      // @ts-ignore
+      modular: true,
+    });
+  });
+
+  it('should set a transformer to transform the data', async () => {
+    const transform = jest.fn<any>();
+    const typeGenerator = new TypeGenerator({ uri: 'https://domain', transform });
+    await typeGenerator.generate();
+    expect(transform).toHaveBeenCalledWith({ a: 1 });
+  });
+});
