b565c2fe00f5db0db71269db433650630aac8cb7
vanpipy <vanpipy@gmail.com>
2025-05-21 10:52:01 +0800

    fix(eslint-config): format the file

--- packages/crowbar-eslint-config/index.js
+++ packages/crowbar-eslint-config/index.js
@@ -1,28 +1,26 @@
+/* eslint-disable */
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'es6',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   rules: {
     'prettier/prettier': [
       'error',
       {
         singleQuote: true,
-        printWidth: 100
-      }
+        printWidth: 100,
+      },
     ],
     'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off'
-  }
+    'prefer-arrow-callback': 'off',
+  },
 };
