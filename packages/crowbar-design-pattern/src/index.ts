d17f439da769854c7a56a853e04ae084e2e4c082
vanpipy <vanpipy@gmail.com>
2025-06-09 15:46:54 +0800

    chore: expose observer and subscriber

--- packages/crowbar-design-pattern/src/index.ts
+++ packages/crowbar-design-pattern/src/index.ts
@@ -1,4 +1,5 @@
 import BehaviorChain, { BREAK_SIGNAL } from './BehaviorChain';
 import Command from './Command';
+import Observer, { Subscriber } from './Observer';
 
-export { BehaviorChain, BREAK_SIGNAL, Command };
+export { BehaviorChain, BREAK_SIGNAL, Command, Observer, Subscriber };
